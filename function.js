2252b884a818ac755c83e95ac34cbaa862528833
Ceci1515 <123477819+Ceci1515@users.noreply.github.com>
2023-01-24 15:36:11 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,4 @@
+document.getElementById("boton").onclick = function () {
+    console.log("Capturamos el evento click");
+    document.getElementById("demo").innerHTML ="JavaScript Ruby Html CSS Front end"
+}
